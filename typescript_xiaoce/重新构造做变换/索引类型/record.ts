d071ba89ba6846237eb388eef53fc30961e243fb
xiaolu-coding <454819375@qq.com>
2022-04-16 16:50:36 +0800

    fix: Index record

--- typescript_xiaoce/重新构造做变换/索引类型/record.ts
+++ typescript_xiaoce/重新构造做变换/索引类型/record.ts
@@ -1,4 +1,7 @@
 type record<K extends string | number | symbol, T> = {
   [P in K ] : T
 }
-
+// 限制obj类型
+type uppercaseKeys1<Obj extends record<string, object>> = {
+  [Key in keyof Obj as Uppercase<Key & string>]: Obj[Key]
+}
