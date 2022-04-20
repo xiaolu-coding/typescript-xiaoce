e032b5f3dcd8191ef32d5b3749db29c9b235f727
xiaolu-coding <454819375@qq.com>
2022-04-20 11:04:10 +0800

    chore: KebabCaseToCamelCase

--- typescript_xiaoce/类型编程综合实战一/KebabCaseToCamelCase.ts
+++ typescript_xiaoce/类型编程综合实战一/KebabCaseToCamelCase.ts
@@ -2,3 +2,5 @@ type KebabCaseToCamelCase<Str extends string> =
   Str extends `${infer Item}-${infer Rest}`
     ? `${Item}${KebabCaseToCamelCase<Capitalize<Rest>>}`
     : Str
+// type KebabCaseToCamelCaseResult = "helloWorldXiaoLu"
+type KebabCaseToCamelCaseResult = KebabCaseToCamelCase<'hello-world-xiao-lu'>
