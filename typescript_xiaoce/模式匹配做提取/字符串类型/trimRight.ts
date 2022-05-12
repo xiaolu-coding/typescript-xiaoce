9e9120b23d8ed8c6a85f06ab129a9e6ca6ad40ef
xiaolu-coding <454819375@qq.com>
2022-05-12 08:07:33 +0800

    chore

--- typescript_xiaoce/模式匹配做提取/字符串类型/trimRight.ts
+++ typescript_xiaoce/模式匹配做提取/字符串类型/trimRight.ts
@@ -2,4 +2,4 @@ type str3 = 'abc    '
 // 使用Str匹配模式串，由Prefix和后面的空字符串组成，匹配成功则递归，否则返回Str
 type trimRight<Str extends string> = Str extends `${infer Pre}${' ' | '\t' | '\n' }` ? trimRight<Pre> : Str
 // type a3 = 'abc'
-type a3 = trimRight<str3>
+type a3 = trimRight<str3> 
